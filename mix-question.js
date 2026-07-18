cc1010f889b45d9758a1c047dbc99d9adfc1e34a
Amit Anand <amit.anand0526@gmail.com>
2026-07-18 22:09:44 +0530

    Refactor largest number function and comment out unused code sections

--- mix-question.js
+++ mix-question.js
@@ -14,7 +14,49 @@ function check(n) {
 
 // 2. Largest of Three Numbers. Take three numbers and print the largest one.
 
-const one = Number(prompt("Enter your first number: "));
-const two = Number(prompt("Enter your second number: "));
-const three = Number(prompt("Enter your third number: "));
+// const one = Number(prompt("Enter your first number: "));
+// const two = Number(prompt("Enter your second number: "));
+// const three = Number(prompt("Enter your third number: "));
 
+function largest() {
+  if (one > two && one > three) {
+    return `${one} is largest`;
+
+  } else if (two > one && two > three) {
+    return `${two} is largest`;
+
+  } else {
+    return `${three} is largest`
+  }
+}
+// console.log(largest());
+  
+// 3. Reverse an Array [1,2,3,4]
+
+const arr = [1, 2, 3, 4];
+
+// for (let i = arr.length-1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+
+// 4. Count Even Numbers .Find how many even numbers are in an array. [2,5,8,9,10]
+
+let arr1 = [2, 5, 8, 9, 10];
+let count = 0
+
+// for (let i = 0; i <= arr1.length; i++) {
+//   if (arr1[i] % 2 === 0) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// 5. Sum of Array [10,20,30].
+
+// let numbers = [10, 20, 30];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++){
+//   sum = sum + numbers[i];
+// }
+//  console.log(sum)
