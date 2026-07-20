6a6cbe4c18f779a2b7b5e06946b797b12643bdf6
Amit Anand <amit.anand0526@gmail.com>
2026-07-20 22:51:28 +0530

    optimize full code

--- Basics/04-operators.js
+++ Basics/04-operators.js
@@ -1,4 +1,4 @@
- /*================= Operators(31–40) ========== */
+/*================= Operators(31–40) ========== */
 
 // 31. Add two numbers.
 const add1 = 4;
@@ -13,7 +13,7 @@ const sub2 = 4;
 // 33. Multiply two numbers.
 const num1 = 7;
 const num2 = 8;
-// console.log(num1 * num2); 
+// console.log(num1 * num2);
 
 // 34. Divide two numbers.
 const num3 = 12;
@@ -23,49 +23,43 @@ const num4 = 2;
 // 35. Find the remainder.
 const one = 13;
 const two = 2;
-// console.log(one % two); 
+// console.log(one % two);
 
 // 36. Increase a variable using ++.
 let a = 3;
 let b = a++;
-// console.log(a); 
-// console.log(b); 
+// console.log(a);
+// console.log(b);
 
 // 37. Decrease a variable using --.
 let c = 5;
-let d = c--; 
+let d = c--;
 // console.log(c);
-// console.log(d); 
+// console.log(d);
 
 // 38. Check if two values are equal.
 const val1 = 4;
 const val2 = 4;
 if (val1 === val2) {
   console.log("equal");
-} 
+}
 
 // 39. Check if two values are equal. (Correction: Check if greater or equal)
 const num5 = 7;
 const num6 = 6;
 if (num5 >= num6) {
   // console.log("greater");
-} 
+}
 // 40. Use logical AND and OR.
 const hasAccount = true;
 const isLoggedIn = false;
 
 // Logical OR (||) needs only one side to be true
 if (hasAccount || isLoggedIn) {
-  console.log("Welcome visitor!"); 
+  console.log("Welcome visitor!");
 }
 
 // Logical AND (&&) needs both sides to be true
 if (hasAccount && !isLoggedIn) {
-  console.log("Please log in to your account."); 
+  console.log("Please log in to your account.");
 }
-
-
-
-
-
-
