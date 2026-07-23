1067730cc57da064b5195cb0cfe39f9f53567c62
Amit Anand <amit.anand0526@gmail.com>
2026-07-23 21:30:14 +0530

    solve 4 question

--- Basics/05-if-else.js
+++ Basics/05-if-else.js
@@ -2,10 +2,44 @@
 
 // 41.Check even or odd.
 const prompt = require('prompt-sync')();
-let check = Number(prompt("enter your number: "));
+// let num = Number(prompt("enter your number: "));
 
-if (check%2==0) {
-  console.log("Even");
-} else {
-  console.log("odd");
- }
+// if (num%2==0) {
+//   console.log("Even");
+// } else {
+//   console.log("odd");
+// }
+ 
+// 42.Check positive or negative
+// let check = Number(prompt("Enter your number: "));
+
+// if (check > 0) {
+//   console.log("Positive");
+// } else if (check === 0) {
+//   console.log("Zero");
+// } else {
+//   console.log("Negative");
+// }
+
+// 43.Find the larger of two numbers.
+// let num1 = Number(prompt("Enter your first number: "));
+// let num2 = Number(prompt("Enter your second number: "));
+
+// if (num1 > num2) {
+//   console.log(`${num1} is larger than ${num2}`);
+// } else {
+//   console.log(`${num2} is larger than ${num1}`);
+// }
+
+// 44.Find the largest of three numbers.
+// let one = Number(prompt("Enter your first number: "));
+// let two = Number(prompt("Enter your second number: "));
+// let three = Number(prompt("Enter your third number: "));
+
+// if ((one>two) && (one>three)) {
+//   console.log(`${one} largest than ${two} and ${three}`);
+// } else if ((two>one) && (two>three)) {
+//    console.log(`${two} largest than ${one} and ${three}`);
+// } else {
+//    console.log(`${three} largest than ${one} and ${two}`);
+// }
