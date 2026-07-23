68521c4ebbb0f52ae7f11395acda611a337abf17
Amit Anand <amit.anand0526@gmail.com>
2026-07-23 21:01:21 +0530

    install node_modules and add if-else

---
+++ Basics/05-if-else.js
@@ -0,0 +1,11 @@
+// ============ if-else(41-50)=================
+
+// 41.Check even or odd.
+const prompt = require('prompt-sync')();
+let check = Number(prompt("enter your number: "));
+
+if (check%2==0) {
+  console.log("Even");
+} else {
+  console.log("odd");
+ }
